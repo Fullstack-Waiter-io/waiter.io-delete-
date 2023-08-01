1bd4bb5cecc933ba3169c947028898f91202016d
calvinxhe <herb.builder@gmail.com>
2023-08-01 15:52:37 -0400

    idk

---
+++ src/MenuCapture.js
@@ -0,0 +1,33 @@
+// import React, { useState } from 'react';
+// import axios from 'axios';
+
+// export default function MenuCapture(){
+//     //things that change
+//     //image
+//     const [image, setImage] = useState(null);
+
+//     //Image changes on target?
+//     const ImageChange = (e) =>{
+//         const newImage= e.target.image[0];
+//         setImage(newImage)
+//     };
+
+//     //read and translate
+//     //input image into ocr
+//     const OCR = async () =>{
+//         try{
+//             const ocrResponse = await axios.post('/api/ocr', image)
+//         }catch (error) {
+//       console.error('Menu issue:', error);
+//         }
+//     }
+
+//     const GPTResponse = async (text)=>{
+//         try{
+//             const chatgpt = await axios.post('/api/openapi', text)
+//         }catch (error) {
+//             console.error('Menu issue:', error);
+//         }
+//     }
+
+// }
